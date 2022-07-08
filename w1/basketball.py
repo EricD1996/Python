fab00b38edb686d36b4fc4bf2867b89a110ce4ea
EricD1996 <EricD8816051@gmail.com>
2022-07-08 11:19:54 -0700

    Basketball Dictionaries assignment

--- w1/basketball.py
+++ w1/basketball.py
@@ -1,10 +1,14 @@
-class  Player:
-    def __init__(self, players['name'], players['age'], players['position'], players['team']):
-        self.name = players['name']
-        self.age = players['age']
-        self.position = players['position']
-        self.team = players['team']
-
+# class  Player:
+#     def __init__(self, players):
+#         self.name = players["name"]
+#         self.age = players['age']
+#         self.position = players['position']
+#         self.team = players['team']
+#     def print_info(self):
+#         print(self.name)
+#         print(self.age)
+#         print(self.position)
+#         print(self.team)
 
 players = [
     {
@@ -45,4 +49,47 @@ players = [
     }
 ]
 
-player1 = Player()
+# player1 = Player(players[0])
+# player1.print_info()
+
+
+# class Player:
+#     def __init__(self, player):
+#         self.name = player['name']
+#         self.age = player['age']
+#         self.position = player['position']
+#         self.team = player['team']
+#     def print_info(self):
+#         print(self.name)
+#         print(self.age)
+#         print(self.position)
+#         print(self.team)
+# kevin = {
+#     	"name": "Kevin Durant", 
+#     	"age":34, 
+#     	"position": "small forward", 
+#     	"team": "Brooklyn Nets"
+# }
+# jason = {
+#     	"name": "Jason Tatum", 
+#     	"age":24, 
+#     	"position": "small forward", 
+#     	"team": "Boston Celtics"
+# }
+# kyrie = {
+#     	"name": "Kyrie Irving", 
+#     	"age":32,
+#         "position": "Point Guard", 
+#     	"team": "Brooklyn Nets"
+# }
+    
+# # Create your Player instances here!
+# player_kevin = Player(kevin)
+# player_kevin.print_info()
+# # player_jason = ???
+
+
+new_team = []
+for x in range(0, len(players)):
+    new_team.append(players[x])
+print(new_team)
