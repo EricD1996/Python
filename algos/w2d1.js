eb4be9a7bfbdd2bd4981b418fe22433d2a6fcfc5
EricD1996 <EricD8816051@gmail.com>
2022-07-11 09:07:12 -0700

    Upload

---
+++ algos/w2d1.js
@@ -0,0 +1,58 @@
+/* 
+  Given an array of strings
+  return the number of times each string occurs (a frequency / hash table)
+*/
+
+const arr1 = ["a", "a", "a"];
+const expected1 = {
+  a: 3,
+};
+
+const arr2 = ["a", "b", "a", "c", "B", "c", "c", "d"];
+const expected2 = {
+  a: 2,
+  b: 1,
+  c: 3,
+  B: 1,
+  d: 1,
+};
+
+const arr3 = [];
+const expected3 = {};
+
+/**
+ * Builds a frequency table based on the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<string>} arr
+ * @returns {Object<string, number>} A frequency table where the keys are items
+ *    from the given arr and the values are the amnt of times that item occurs.
+ */
+function makeFrequencyTable(arr) {
+    var newObject = {};
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] in newObject){
+            newObject[arr[i]] += 1;
+        } else {
+            newObject[arr[i]] = 1;
+        }
+    }
+    return newObject;
+}
+console.log(makeFrequencyTable(arr3))
+
+const two_nums1 = [1];
+const two_expected1 = 1;
+
+const two_nums2 = [5, 4, 5];
+const two_expected2 = 4;
+
+const two_nums3 = [5, 4, 3, 4, 3, 4, 5];
+const two_expected3 = 4; // there is a pair of 4s but one 4 has no pair.
+
+const two_nums4 = [5, 2, 6, 2, 3, 1, 6, 3, 2, 5, 2];
+const two_expected4 = 1;
+
+function oddOccurrencesInArray(nums) {
+    
+}
