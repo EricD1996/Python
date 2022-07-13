45707876a30b27d3a50f81cd60a35e943fde9c66
EricD1996 <EricD8816051@gmail.com>
2022-07-13 10:58:33 -0700

    update counter

--- flask/counter/counter.py
+++ flask/counter/counter.py
@@ -8,13 +8,20 @@ def counter():
     
 @app.route('/count', methods=['POST'])
 def add_counter():
-    session['number'] = 0
-    session['number'] += 3
+    if not 'number' in session:
+        session['number'] = 1
+    if 'number' in session:
+        session['number'] += 1
     return redirect('/redirect')
 
 @app.route('/redirect')
 def redirected():
     return render_template('count.html')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
